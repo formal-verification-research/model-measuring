1704e8ed69247b3f573b531870d7ed0e9b21e38a
stocktonslack <stocktonslack@el176-student1.ece.usu.edu>
2020-02-03 09:30:35 -0700

    Generating hypervisor in progress

--- HypervisorSlack.py
+++ HypervisorSlack.py
@@ -13,8 +13,10 @@ bdict = spot.make_bdd_dict();
 #makes an empty automaton 
 buchiHypervisor = spot.make_twa_graph(bdict); 
 
-test1 = buddy.bdd_ithvar(buchiHypervisor.register_ap("test1"))
-test2 = buddy.bdd_ithvar(buchiHypervisor.register_ap("test2"))
+true = buddy.bdd_ithvar(buchiHypervisor.register_ap("true"))
+label = buddy.bdd_ithvar(buchiHypervisor.register_ap("ltl"))
+notLabel = buddy.bdd_ithvar(buchiHypervisor.register_ap("!ltl"))
+transitionLabel = buddy.bdd_ithvar(buchiHypervisor.register_ap("transition"))
 
 buchiHypervisor.set_generalized_buchi(2)
 
@@ -22,7 +24,13 @@ buchiHypervisor.new_states(3)
 
 buchiHypervisor.set_init_state(0)
 
-buchiHypervisor.new_edge(0,1, test1)
+buchiHypervisor.new_edge(0,0, true)
+buchiHypervisor.new_edge(0,1, transitionLabel)
+buchiHypervisor.new_edge(1,1, label)
+buchiHypervisor.new_edge(1,1, notLabel)
+buchiHypervisor.new_edge(1,0, label)
+
+
 
 print(buchiHypervisor.to_str('hoa'))
 
@@ -32,3 +40,9 @@ buchiHypervisor.save('buchiHypervisor.dot','dot')
 ret_code = subprocess.call(['dot', '-Tpdf', 'buchiHypervisor.dot', '-o', 'buchiHypervisor.pdf'])
 if ret_code == 0:
     print('Human readable Buchi automaton saved at buchiHypervisor.pdf')
+
+
+
+
+#Can we just use a single transition to change from the idle state to the faulty state?
+#Can't we have several places that could transition into an area where we would want to be faulty?
